1a21bc7d1b4b352684c5371fd72467c4b33d918f
Faisal Muhammad Adam <developerfaisalm93@gmail.com>
2023-02-14 23:15:23 +0100

    Add files via upload

---
+++ Favorite_book.py
@@ -0,0 +1,4 @@
+def favorite_book(title):
+    print("One of my favorite books is " + title + ".")
+
+favorite_book("Python Data Science: An Ultimate Guide for Beginners to Learn Fundamentals of Data Science Using Python. The Book is written by Christopher Wilkinson")
